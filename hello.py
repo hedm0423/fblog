a452dfa6b00e5ddbb4a0d911034a80c225371ecf
hedamin <hedamin@linux-a11w.DHCP>
2017-02-04 21:37:22 +0800

    initialization

---
+++ hello.py
@@ -0,0 +1,16 @@
+#from flask.ext.boostrap import Bootstrap
+from flask import Flask
+app = Flask(__name__)
+
+#bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+	return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, %s</h1>' %name
+
+if __name__ == '__main__':
+	app.run(debug=True)
